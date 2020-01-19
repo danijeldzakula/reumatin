5abe0992c65afe0c01af5d1c97c0a5da9cfb755d
Dado19Git <danidzaki060@gmail.com>
2020-01-19 18:37:53 +0100

    First commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+﻿function ScrollIndicator() {
+	var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+	var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	var scrolled = (winScroll / height) * 100;
+	document.getElementById('scrollIndicator').style.width = scrolled + "%";
+}
+
+window.addEventListener('scroll', ScrollIndicator);
